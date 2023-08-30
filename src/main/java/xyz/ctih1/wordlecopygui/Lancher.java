22cd7c4a6e38369270fb1364f0b77c8b8481a8a5
ctih <nevalaonni@proton.me>
2023-08-30 19:17:34 +0300

    Fixed minor bugs

---
+++ src/main/java/xyz/ctih1/wordlecopygui/Lancher.java
@@ -0,0 +1,9 @@
+package xyz.ctih1.wordlecopygui;
+
+import java.io.IOException;
+
+public class Lancher {
+    public static void main(String[] args) throws IOException {
+        GUI.main(args);
+    }
+}
